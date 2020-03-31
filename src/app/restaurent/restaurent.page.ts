b7fff46994c7d758b05224287c3ae93d4a9389d6
devloper <rksury@gmail.com>
2020-03-31 01:17:39 -0400

    update restaurent page

--- src/app/restaurent/restaurent.page.ts
+++ src/app/restaurent/restaurent.page.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
-  selector: 'app-restaurent',
-  templateUrl: './restaurent.page.html',
-  styleUrls: ['./restaurent.page.scss'],
+    selector: 'app-restaurent',
+    templateUrl: './restaurent.page.html',
+    styleUrls: ['./restaurent.page.scss'],
 })
 export class RestaurentPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {
+  }
 
   ngOnInit() {
   }
 
+  restaurant_detail() {
+    this.router.navigate(['/tabs/restdetail']);
+  }
 }
